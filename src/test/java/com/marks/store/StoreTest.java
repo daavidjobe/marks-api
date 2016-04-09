09870e1b9737ef6bec907c972ed415fca1c8be5d
daavidjobe <daavid.jobe@gmail.com>
2016-04-09 19:13:47 +0200

    Mongo configured

--- src/test/java/com/marks/store/StoreTest.java
+++ src/test/java/com/marks/store/StoreTest.java
@@ -1,5 +1,7 @@
 package com.marks.store;
 
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,7 +14,7 @@ public class StoreTest {
 
     @Before
     public void setUp() {
-        store = new Store("test-db");
+        store = new Store("marksDB-test");
     }
 
     @After
@@ -23,7 +25,18 @@ public class StoreTest {
 
     @Test
     public void shouldReturnCorrectDatabaseName() throws Exception {
-        assertEquals("test-db", store.getDatabase().getName());
+        assertEquals("marksDB-test", store.getDatabase().getName());
+    }
+
+    @Test
+    public void shouldInsertDocument() throws Exception {
+        MongoCollection<Document> collection = store.getDatabase().getCollection("test");
+        Document doc = new Document("name", "MongoDB")
+                .append("type", "database")
+                .append("count", 1)
+                .append("info", new Document("x", 203).append("y", 102));
+        collection.insertOne(doc);
+        assertEquals(collection.count(), 1);
     }
 
 
