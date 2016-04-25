72e208a668464648274ced78cb24b50e89b53099
daavidjobe <daavid.jobe@gmail.com>
2016-04-25 18:55:15 +0200

    -

--- src/main/java/com/marks/Main.java
+++ src/main/java/com/marks/Main.java
@@ -7,18 +7,28 @@ import com.marks.util.Config;
 import org.apache.log4j.Logger;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class Main {
 
     final static Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         Store.initialize(Config.DB_PROD);
 
-        UserController userController = new UserController(new UserService());
+        new UserController(new UserService());
 
         get("/api/", (req, res) -> "Marks REST API");
 
 
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
     }
 }
