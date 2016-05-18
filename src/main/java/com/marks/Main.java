871123826d5d3631696db76e803f6b6918b74b98
daavidjobe <daavid.jobe@gmail.com>
2016-05-18 11:20:41 +0200

    -

--- src/main/java/com/marks/Main.java
+++ src/main/java/com/marks/Main.java
@@ -26,7 +26,7 @@ public class Main {
 
         get(Config.ROOT_PATH, (req, res) -> "Marks REST API");
 
-        get(Config.ROOT_PATH + "/webSocketUrl", (req, res) -> "ws://" + Config.BASE_URL + ":" + Config.PORT + "/api/socket/");
+        get(Config.ROOT_PATH + "/webSocketUrl", (req, res) -> "ws://" + Config.BASE_URL + ":" + Config.PORT + "/api/socket");
 
     }
 
